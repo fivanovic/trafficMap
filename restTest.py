65c944d75c5c8f0848e0f2bd21da528c6ee3d2e2
filiivan <filip.ivanovic@keysight.com>
2025-02-18 19:19:45 +0100

    TIME BITCH

--- restTest.py
+++ restTest.py
@@ -1,6 +1,7 @@
 import requests
-
+import pprint
 import json
+import pandas as pd
 
 
 s = requests.Session()
@@ -21,11 +22,28 @@ myrep = s.get(url+f"location.name?input=eternitten&type=S&name=Bus 2&maxNo=2")
 #851030703
 #A=1@O=Eternitten (Østre Allé / Aalborg)@X=9937054@Y=57038111@U=86@L=851030703@B=1@p=1739790541
 #A=1@O=Eternitten (Bornholmsgade / Aalborg)@X=9935795@Y=57038228@U=86@L=851008902@
-myrep = s.get(url+f"multiArrivalBoard?idList=A=1@O=Eternitten (Bornholmsgade / Aalborg)@X=9935795@Y=57038228@U=86@L=851008902@&date=2025-02-17&time=08:00&duration=8")
+myrep = s.get(url+f"multiArrivalBoard?idList=A=1@O=Eternitten (Bornholmsgade / Aalborg)@X=9935795@Y=57038228@U=86@L=851008902@&date=2025-02-20&time=08:00&duration=60")
 #print(myrep.json())
-jdict=myrep.json()
-finout = json.dumps(jdict)
-criteria = {"name":"Bybus 2"}
+fdict=myrep.json()
+
+def filterdic(pairs):
+    key,value = pairs
+    filter_value = "2"
+    if value == filter_value:
+        return True
+    else:
+        return False
+
+l1=fdict["Arrival"]
+
+i=0
+for x in l1:
+    if l1[i]["name"] == "Bybus 2":
+        pp = pprint.PrettyPrinter(depth=4)
+        pp.pprint(l1[i]["time"])
+    i = i+1
+
+#stops = [entries for entries in jdict if entries["name"] == "Bybus 2"]
 
 
 #outdict = [x for x in jdict if x[&quot;age&quot;&gt;] "Bybus 2"]
