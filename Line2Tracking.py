5bfe8efe90bc9d9dc92200b6c269530cc6928899
filiivan <filip.ivanovic@keysight.com>
2025-03-01 16:36:27 +0100

    able to say when the next 2 is

--- Line2Tracking.py
+++ Line2Tracking.py
@@ -2,27 +2,52 @@ import requests
 import pprint
 import json
 import pandas as pd
+import numpy as np
+from datetime import datetime 
+from Line2 import ett
+import timedelta as td
+import time
 
-from Line2 import vdb, ett
+day = datetime.today().strftime('%Y-%m-%d')
+tim = datetime.today().strftime('%H:%M')
 
 s = requests.Session()
 s.headers.update({'Authorization': 'Bearer deb79eea-1eee-430d-9632-1e987aa46c15'})
 s.headers.update({'Accept': 'application/json'})
 url = f"https://www.rejseplanen.dk/api/"
 
-#myrep = s.get(url+f"multiArrivalBoard?idList="+ vdb.upcode + f"&date=2025-02-20&time=08:00&duration=60")
-myrep = s.get(url+f"multiArrivalBoard?idList="+vdb.upcode+"|"+ett.upcode+f"&date=2025-02-20&time=10:00&duration=10")
-fdict=myrep.json()
-l1=fdict["Arrival"]
-pp = pprint.PrettyPrinter(depth=4)
+#myrep = s.get(url+f"multiArrivalBoard?idList="+ett.downcode+"|"+ett.upcode+f"&date="+day+f"&time="+tim+"&duration=10")
+#fdict=myrep.json()
+#l1=fdict["Arrival"]
+#pp = pprint.PrettyPrinter(depth=4)
 #pp.pprint(l1)
-i=0
 
+nextBusUp = np.array([">10", ">10", ">10"])
+nextBusDown = np.array([">10", ">10", ">10"])
 
-#this is going to be the upstream method for IDing 
-for x in l1:
-    if (l1[i]["origin"].startswith("AAU Bus")) or (l1[i]["origin"].startswith("Aalborg Universitets")): 
-        pp = pprint.PrettyPrinter(depth=4)
-        pp.pprint(l1[i]["stop"])
-        pp.pprint(l1[i]["time"])
-    i = i+1
+while 1:
+    day = datetime.today().strftime('%Y-%m-%d')
+    tim = datetime.today().strftime('%H:%M')
+    deltaT = td.Timedelta()
+    myrep = s.get(url+f"multiArrivalBoard?idList="+ett.downcode+"|"+ett.upcode+f"&date="+day+f"&time="+tim+"&duration=30")
+    fdict=myrep.json()
+    ettBoard = fdict["Arrival"]
+    i = 0
+    j= 0
+    for x in ettBoard:
+        if (ettBoard[i]["origin"].startswith("AAU Bus")) or (ettBoard[i]["origin"].startswith("Aalborg Universitets")):
+            ettUpArrN = ettBoard[i]["name"]
+            ettUpArrT = ettBoard[i]["time"]
+            ettUpArrT = datetime.strptime(ettUpArrT,'%H:%M:%S')
+            if ettUpArrN == "Bybus 2":
+                arrD = td.Timedelta(ettUpArrT - datetime.strptime(tim,'%H:%M'))
+                if arrD >= deltaT:
+                    nextTwoUp = str(arrD.total.minutes)
+                    upField = f"The next 2 towards town is in " + nextTwoUp + f" minutes"
+                    print ("\r" + upField + "        ", end='')
+                    break
+        i = i+1
+
+ 
+    time.sleep(60)
+
--- restTest.py
+++ restTest.py
@@ -2,30 +2,31 @@ import requests
 import pprint
 import json
 import pandas as pd
+from datetime import datetime 
+import numpy as np
+from Line2 import ett
 
-
+day = datetime.today().strftime('%Y-%m-%d')
+tim = datetime.today().strftime('%H:%M')
 s = requests.Session()
 s.headers.update({'Authorization': 'Bearer deb79eea-1eee-430d-9632-1e987aa46c15'})
 s.headers.update({'Accept': 'application/json'})
 url = f"https://www.rejseplanen.dk/api/"
 #access = f"accessId=deb79eea-1eee-430d-9632-1e987aa46c15"
-#myrep = s.get(url+f"linematch?match=2&operators=NT")
-#print(myrep.headers)
-#print(myrep.json())
-
-#myrep = s.get(url+f"lineinfo?lineId=028_280_06_2&date=2025-02-17")
-#print(myrep.json())
-
-myrep = s.get(url+f"location.name?input=Væddeløbsbanen&type=S")
-pp = pprint.PrettyPrinter(depth=4)
+#myrep = s.get(url+f"lineinfo?lineId=028_280_06_2&date="+day)
+#lineInf= myrep.json()
+#pp = pprint.PrettyPrinter(depth=4)
+#pp.pprint(lineInf)
+#myrep = s.get(url+f"location.name?input=Eternitten&type=S")
+#pp = pprint.PrettyPrinter(depth=4)
 #pp.pprint(myrep.json())
 
 #851030703
-ost = "A=1@O=Eternitten (Østre Allé / Aalborg)@X=9937054@Y=57038111@U=86@L=851030703@B=1@p=1739790541"
+#ost = "A=1@O=Eternitten (Østre Allé / Aalborg)@X=9937054@Y=57038111@U=86@L=851030703@B=1@p=1739790541"
 born = "A=1@O=Eternitten (Bornholmsgade / Aalborg)@X=9935795@Y=57038228@U=86@L=851008902@"
-ved = "A=1@O=Væddeløbsbanen (Skydebanevej / Aalborg)@X=9881159@Y=57054184@U=86@L=851007301@B=1@p=1739892283@"
-jom = "A=1@O=Jomfru Ane Gade (Borgergade / Aalborg)@X=9918212@Y=57050957@U=86@L=851002402@B=1@p=1739892283@"
-myrep = s.get(url+f"multiArrivalBoard?idList="+born+f"&date=2025-02-20&time=08:00&duration=60")
+#ved = "A=1@O=Væddeløbsbanen (Skydebanevej / Aalborg)@X=9881159@Y=57054184@U=86@L=851007301@B=1@p=1739892283@"
+#jom = "A=1@O=Jomfru Ane Gade (Borgergade / Aalborg)@X=9918212@Y=57050957@U=86@L=851002402@B=1@p=1739892283@"
+myrep = s.get(url+f"multiArrivalBoard?idList="+born+f"&date="+day+f"&time="+tim+f"&duration=10")
 #print(myrep.json())
 fdict=myrep.json()
 
@@ -42,6 +43,13 @@ for x in l1:
         pp.pprint(l1[i]["time"])
     i = i+1
 """
+
+nextBusUp = np.array([">10", ">10", ">10"])
+nextBusDown = np.array([">10", ">10", ">10"])
+
+
+#while(1):
+
 #stops = [entries for entries in jdict if entries["name"] == "Bybus 2"]
 
 
